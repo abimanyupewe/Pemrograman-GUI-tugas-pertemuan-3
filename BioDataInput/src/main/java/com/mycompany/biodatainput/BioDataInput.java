82d4381f850a78b3a71c36e3a8d540df7ceb8b97
abimanyupewe <abimanyupw369@gmail.com>
2024-10-15 20:50:17 +0700

    pertemuan 3

---
+++ BioDataInput/src/main/java/com/mycompany/biodatainput/BioDataInput.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.biodatainput;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class BioDataInput {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
